30febcd6ee913985e04d9b7c699cdf128f9e7517
César Chávez <79390377+CsarChvz@users.noreply.github.com>
2026-06-24 14:21:21 -0600

    lol

--- terraform/00-main.tf
+++ terraform/00-main.tf
@@ -10,10 +10,10 @@ terraform {
     }
   }
 
-  # backend "s3" {
-  #   bucket  = "notification-service-expdynts-terraform-state"
-  #   key     = "terraform.tfstate"
-  #   region  = "us-east-1"
-  #   encrypt = true
-  # }
+  backend "s3" {
+    bucket  = "notification-service-expdynts-terraform-state"
+    key     = "terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
 }
